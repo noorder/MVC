a65180790910f64407ff1654799d42613098edd0
Stas Chekalaev <schekalaev@gmail.com>
2021-12-10 01:34:26 +0300

    1

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+require 'application/lib/Dev.php'; //подключаю обработчик ошибок и вывод массива
+
+use application\core\Router; //подклчаю класс рутер
+
+
+spl_autoload_register(function ($class) { //функция срабатывает до вывода ошибки
+    $path = str_replace('\\', '/', $class . '.php');
+
+    if (file_exists($path)) {
+        require $path;
+    }
+});
+
+session_start();
+
+$router = new Router;
+$router->run();
