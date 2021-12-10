65fcc28dbdeadc3ed19903f5b24c59b04f16e8ca
Stas Chekalaev <schekalaev@gmail.com>
2021-12-11 01:48:48 +0300

    1

---
+++ application/config/routes.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'account/login' => [
+        'controller' => 'account',
+        'action' => 'login',
+    ],
+
+    'news/show' => [
+        'controller' => 'news',
+        'action' => 'show',
+    ],
+
+
+];
